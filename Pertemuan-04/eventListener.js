282751efd6d982d0a846c21672431ee57a2cf69b
Nickson Kiyoshi <125760127+EndlessWay1@users.noreply.github.com>
2026-09-20 16:04:39 +0700

    Added Readme, dropdown, footer, and merge work section (#10)
    * working on progress on follow mouse trail using canvas
    
    * added follow mouse feature to the website
    
    * added dropdown feat, footer
    
    * added readme

--- Pertemuan-04/eventListener.js
+++ Pertemuan-04/eventListener.js
@@ -1,4 +1,22 @@
+// CONSTANTS
+const deltaUpdateCanvasToWindow = 20;
+const deltaUpdateMouseMoved = 10;
+
+// Element Constants
 const head = document.getElementById("header");
+const mediaQuery = window.matchMedia("(width >= 992px)");
+const dropdown = document.getElementById("dropdown");
+
+dropdown.addEventListener("click", () => {
+  const dropContentsStyle = getComputedStyle(
+    document.getElementById("dropContent"),
+  ).display;
+  if (dropContentsStyle !== "none") {
+    document.getElementById("dropContent").style.display = "none";
+  } else {
+    document.getElementById("dropContent").style.display = "inline";
+  }
+});
 
 head.addEventListener("animationend", () => {
   if (head.classList.contains("header-moved-top")) {
@@ -18,3 +36,55 @@ window.addEventListener("scroll", (event) => {
     head.classList.remove("header-moved-top");
   }
 });
+
+mediaQuery.addEventListener("change", (e) => {
+  if (e.matches) {
+    document.getElementById("dropContent").style.display = "none";
+  }
+});
+
+// for the follow mouse effects
+const mouseCnvs = new mouseCanvas(20);
+
+const redraw = () => {
+  mouseCnvs.clear();
+  mouseCnvs.createAllBox();
+  requestAnimationFrame(redraw);
+};
+
+// When window is resized, the canvas size gets updated
+let prevW = window.innerWidth;
+let prevH = window.innerHeight;
+
+window.addEventListener("resize", () => {
+  if (
+    Math.abs(prevH - window.innerHeight) >= deltaUpdateCanvasToWindow ||
+    Math.abs(prevW - window.innerWidth) >= deltaUpdateCanvasToWindow
+  ) {
+    prevW = window.innerWidth;
+    prevH = window.innerHeight;
+    mouseCnvs.resizeCanvas();
+  }
+});
+
+// follow the mouse
+let prevMouseX = -1;
+let prevMouseY = -1;
+window.addEventListener("mousemove", (e) => {
+  const x = e.clientX;
+  const y = e.clientY;
+  if (
+    Math.abs(prevMouseX - x) >= deltaUpdateMouseMoved ||
+    Math.abs(prevMouseY - y) >= deltaUpdateMouseMoved
+  ) {
+    mouseCnvs.recordMouse(x, y, prevMouseX, prevMouseY);
+    prevMouseX = x;
+    prevMouseY = y;
+  }
+});
+
+setInterval(() => {
+  mouseCnvs.updateArray();
+}, 100);
+
+redraw();
