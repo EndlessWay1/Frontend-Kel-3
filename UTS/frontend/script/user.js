a4d46e567e54c20f503cb79a0694957e5096d5fc
Nickson Kiyoshi <125760127+EndlessWay1@users.noreply.github.com>
2026-09-26 17:01:28 +0700

    Set navbar to fixed, make login.html, and make user.html (#22)
    * set navbar to fixed, make login.html, and make user.html

--- UTS/frontend/script/user.js
+++ UTS/frontend/script/user.js
@@ -1,16 +1,36 @@
 // check auth
+import { formatDistanceToNow } from "https://cdn.jsdelivr.net/npm/date-fns@4.4.0/+esm";
 
-async function Profile() {
+function Profile() {
   const user = auth();
 
   const outerDiv = $("<div id='user-info'></div>");
 
-  if (user) {
+  if (!user) {
     window.location.href = "./login.html";
   }
 
-  //   (<div id=""></div>)
-
+  outerDiv.append(`<div
+    id="profile"
+    class="card"
+  >
+    <div class="card-body">
+        <h4 class="card-title text-center fs-2">User Profile</h4>
+        <p class='text-center'><span>Joined since ${formatDistanceToNow(user.joined, { addSuffix: true })}</span></p>
+        <div class='profile-content'>
+            <h5>Username</h5>
+            <div class="container-fluid">
+                ${user.username}
+            </div>
+        </div>
+        <div class='profile-content'>
+            <h5>Email</h5>
+            <div class="container-fluid">
+                ${user.email}
+            </div>
+        </div>
+    </div>
+  </div>`);
   return outerDiv;
 }
 
